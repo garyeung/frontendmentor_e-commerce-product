8ca3471d2d2095d7a03fc75202b853ce75588cca
garyeung <5206130+garyeung@users.noreply.github.com>
2025-12-26 16:30:19 +0800

    create cartItem interface and Product interface

---
+++ my-app/src/interfaces/CartItem.ts
@@ -0,0 +1,6 @@
+interface ICartItem {
+    id: number,
+    quantity: number,
+}
+
+export default ICartItem;
---
+++ my-app/src/interfaces/Product.ts
@@ -0,0 +1,11 @@
+interface IProduct {
+    id: number,
+    name: string,
+    brand: string,
+    description: string,
+    price: number,
+    discount: number,
+    pictures: string[],
+}
+
+export default IProduct;
