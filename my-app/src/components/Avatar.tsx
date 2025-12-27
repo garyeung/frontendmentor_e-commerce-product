00a36cc71910df7104118a1f436107656e6c448a
garyeung <5206130+garyeung@users.noreply.github.com>
2025-12-27 10:03:40 +0800

    replace onMouse event to onClick

--- my-app/src/components/Avatar.tsx
+++ my-app/src/components/Avatar.tsx
@@ -5,8 +5,15 @@ import './Avatar.less';
 function Avatar({avatarUrl}:{avatarUrl: string}){
     const [active, setActive]= useState(false);
 
+    const handleClick = () =>{
+        setActive(!active);
+    }
+
     return(
-        <div className={`avatar ${active? 'avatar--active': ""}`} onMouseDown={()=>setActive(true)} onMouseUp={()=>setActive(false)}> 
+        <div 
+        className={`avatar ${active? 'avatar--active': ""}`}
+        onClick={handleClick}
+         > 
             <img src={avatarUrl} alt="avatar"/>
         </div>
     )
