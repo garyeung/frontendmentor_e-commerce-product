c3c2ee647f84f95ee893536a9ab0589802e05254
garyeung <5206130+garyeung@users.noreply.github.com>
2025-12-27 10:09:50 +0800

    implement useClickOutside

--- my-app/src/components/Avatar.tsx
+++ my-app/src/components/Avatar.tsx
@@ -1,16 +1,25 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './Avatar.less';
+import { useClickOutside } from '@/hooks/useClickOutside';
 
 
 function Avatar({avatarUrl}:{avatarUrl: string}){
     const [active, setActive]= useState(false);
+    const avatarRef = useRef<HTMLDivElement>(null)
+
+    useClickOutside(avatarRef, ()=> {
+        setActive(false);
+    })
 
     const handleClick = () =>{
         setActive(!active);
     }
 
+    
+
     return(
         <div 
+        ref={avatarRef}
         className={`avatar ${active? 'avatar--active': ""}`}
         onClick={handleClick}
          > 
