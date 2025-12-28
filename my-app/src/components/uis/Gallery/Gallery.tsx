72ab6578123160e3d7d7f84548ce0d6b6ce3d8cd
garyeung <5206130+garyeung@users.noreply.github.com>
2025-12-28 11:26:15 +0800

    rename

--- my-app/src/components/uis/Gallery/Gallery.tsx
+++ my-app/src/components/uis/Gallery/Gallery.tsx
@@ -72,7 +72,7 @@ function Gallery({pictures, active, pushActive, productName}:GalleryProps) {
                     handlePre={() => handleActive(ActionType.add)} 
                     activeIcon={<IconNextActive/>} primaryIcon={<IconNext/>}/>
                 </div>
-                <a className="gallery__mainimg"onClick={()=>{setOpenLightbox(true)}}>
+                <a className="gallery__mainPic"onClick={()=>{setOpenLightbox(true)}}>
                     <img src={mainImg} alt={productName}/>
                 </a>
             </div>
