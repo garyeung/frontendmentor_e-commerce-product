8c3abaab9d0170aedba15f124dc1c1f6762ad566
garyeung <5206130+garyeung@users.noreply.github.com>
2025-12-28 11:20:08 +0800

    import Button

--- my-app/src/components/uis/Gallery/Gallery.tsx
+++ my-app/src/components/uis/Gallery/Gallery.tsx
@@ -1,15 +1,16 @@
 
 import { ActionType } from '@/service';
 import './Gallery.less';
-import iconNext from '@/assets/images/icon-next.svg';
-import iconNextActive from '@/assets/images/icon-next-active.svg'
-import iconPreActive from '@/assets/images/icon-previous-active.svg';
-import iconPre from '@/assets/images/icon-previous.svg';
+import IconNext from '@/assets/images/icon-next.svg?react';
+import IconNextActive from '@/assets/images/icon-next-active.svg?react'
+import IconPreActive from '@/assets/images/icon-previous-active.svg?react';
+import IconPre from '@/assets/images/icon-previous.svg?react';
 import iconClose from '@/assets/images/icon-close-light.svg';
 import iconCloseActive from '@/assets/images/icon-close-active.svg';
 
 import { useState } from 'react';
 import Thumbnail from './Thumbnail';
+import Button from './Button';
 
 export interface GalleryProps {
 pictures: string[], active:number, pushActive: (n:number) => void,
@@ -19,13 +20,9 @@ productName: string,
 function Gallery({pictures, active, pushActive, productName}:GalleryProps) {
     const [openLightbox, setOpenLightbox] = useState(false);
     
-    const iconNexts = [iconNext, iconNextActive];
     const iconCloses = [iconClose, iconCloseActive];
-    const iconPres = [iconPre, iconPreActive];
 
-    const [nextUrl, setnextUrl] = useState(iconNexts[0]);
     const [closeUrl, setCloseUrl] = useState(iconCloses[0]);
-    const [preUrl, setPreUrl] = useState(iconPres[0]);
 
     const thumbnails = pictures.map((picture, index) => {
         return <Thumbnail currentPath={picture} key={productName + index} name={productName+index} handleCurrent={()=> pushActive(index)} active={active} current={index}/>
@@ -55,8 +52,6 @@ function Gallery({pictures, active, pushActive, productName}:GalleryProps) {
         }
     }
     const handleClose = handleIconActive(setCloseUrl, iconCloses)
-    const handlePre = handleIconActive(setPreUrl, iconPres);
-    const handleNext = handleIconActive(setnextUrl, iconNexts);
 
     const mainImg = pictures[active];
     const lightboxClose =   <div className='lightbox__closeboard'>
@@ -68,8 +63,14 @@ function Gallery({pictures, active, pushActive, productName}:GalleryProps) {
             {openLightbox? lightboxClose: <></>}
             <div className="gallery__main">
                 <div className='gallery__buttons'>
-                    <button className='gallery__pre gallery__button' onClick={()=>handleActive(ActionType.reduce)} {...handlePre}><img alt='previous' src={preUrl}/></button>
-                    <button className='garrlery__next gallery__button' onClick={() => handleActive(ActionType.add)}{...handleNext}><img alt='next' src={nextUrl} /></button>
+                    <Button 
+                    handlePre={() => handleActive(ActionType.reduce)} 
+                    activeIcon={<IconPreActive/>} 
+                    primaryIcon={<IconPre/>}
+                    />
+                    <Button 
+                    handlePre={() => handleActive(ActionType.add)} 
+                    activeIcon={<IconNextActive/>} primaryIcon={<IconNext/>}/>
                 </div>
                 <a className="gallery__mainimg"onClick={()=>{setOpenLightbox(true)}}>
                     <img src={mainImg} alt={productName}/>
