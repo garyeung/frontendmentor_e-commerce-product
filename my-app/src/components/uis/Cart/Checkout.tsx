1413b183ec1e041d61a4a856b5483ea7b697c8c6
garyeung <5206130+garyeung@users.noreply.github.com>
2025-12-30 11:03:11 +0800

    seperate Checkout and Empty

---
+++ my-app/src/components/uis/Cart/Checkout.tsx
@@ -0,0 +1,9 @@
+export default function Checkout() {
+    const checkout = () =>{
+        //sample
+    };
+
+    return (<button className="cart__checkout" onClick={checkout}>Checkout</button>);
+
+
+}
